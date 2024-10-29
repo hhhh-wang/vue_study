015a9b7f4a24517988741b2b8366a5c5fafd661f
wangkun <kun64855@gmail.com>
2024-10-29 19:56:15 +0800

    day3

---
+++ Lesson04/code/study.js
@@ -0,0 +1,26 @@
+// 引用类型与值类型
+// let obj = {};
+// let obj2 = {};
+// let obj3 = obj;
+// console.log(obj === obj2);// false
+// console.log(obj === obj3);// true
+
+// function fn(){
+// 	return {
+//
+// 	}
+// }
+// const obj = fn();
+// const obj2 = fn();
+// console.log(obj === obj2);// false
+
+// let obj = {};
+// function fn(){
+// 	return obj;
+// }
+// const obj2 = fn();
+// const obj3 = fn();
+// console.log(obj2 === obj3);
+// obj.a = 100;
+// console.log(obj2,obj3);
+// 金庸-->鹿鼎记-->韦小宝-->七个
